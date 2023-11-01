cfdaf8eb66115b2d8f56d37fcbca771fcc561577
KevinDevelopment <kevincristhian@outlook.com>
2023-11-01 20:31:27 -0300

    :sparkles: feat: add nginx reverse proxy

--- src/presentation/web/express.ts
+++ src/presentation/web/express.ts
@@ -9,10 +9,20 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }));
 app.use(router);
 
 const PORT = 8080;
+
+app.use((req, res, next) => {
+    // Verifique se o cabeçalho personalizado está presente
+    if (req.headers['x-using-nginx'] === 'true') {
+        console.log('Acesso via Nginx.');
+    } else {
+        console.log('Acesso direto ao servidor Node.js.');
+    }
+    next();
+});
 
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`);
