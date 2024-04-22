aac0dafe2f72dd7dffe3c1101e4ceaa858d6bfc8
KevinDevelopment <kevincristhian@outlook.com>
2024-04-22 19:50:40 -0300

    teste

--- src/presentation/web/express.ts
+++ src/presentation/web/express.ts
@@ -18,4 +18,4 @@ const PORT = 8080
 app.get("/", (req: Request, res: Response) => res.json("Servidor funcionando normalmente") )
 app.listen(PORT, () => {
     console.log(`Server running in port ${PORT}`)
-});
+})
